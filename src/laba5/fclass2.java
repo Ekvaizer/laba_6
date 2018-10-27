e6036ad458a5e2b2216e9be5d632a66b2670439e
Alexander Kulinich <380680188847@yandex.ua>
2018-10-27 12:14:50 +0300

    Update fclass2.java

--- src/laba5/fclass2.java
+++ src/laba5/fclass2.java
@@ -2,6 +2,6 @@ package laba5;
 
 public class fclass2 extends  fclass3{
 public void srt() {
-	System.out.println("Клас 2 наследуем от класа 3");
+System.out.println("РљР»Р°СЃ 2 РЅР°СЃР»РµРґСѓРµРј РѕС‚ РєР»Р°СЃР° 3");
 }
 }
